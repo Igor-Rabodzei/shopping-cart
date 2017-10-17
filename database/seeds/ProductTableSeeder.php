863f9d1f349134c468552158082dad7bb8a5b4e2
Igor Rabodzei <igor.rabodzei@gmail.com>
2017-10-17 18:54:54 +0300

    second

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -14,40 +14,47 @@ class ProductTableSeeder extends Seeder
         $product = new App\Product([
             'imagePath' => 'http://static.librebook.me/uploads/pics/01/49/841.jpg',
             'title' => 'Smoke Bellew',
-            'description' => 'super cool',
+            'description' => 'Lorem ipsum dolor sit amet, soleat ridens te vis, blandit maiestatis interpretaris usu ei. Pri expetenda vituperatoribus ad, at probo incorrupte inciderint eos. Rebum dissentiunt nec at, ad pro soluta concludaturque, eu vis noster appareat platonem.',
             'price' => 12
+        ]);
+        $product->save();
+
+        $product = new App\Product([
+            'imagePath' => 'http://read-online.in.ua/inf/book/book453/imgbig453.png',
+            'title' => 'Life is a dream',
+            'description' => 'Lorem ipsum dolor sit amet, soleat ridens te vis, blandit maiestatis interpretaris usu ei. Pri expetenda vituperatoribus ad, at probo incorrupte inciderint eos. Rebum dissentiunt nec at, ad pro soluta concludaturque, eu vis noster appareat platonem.',
+            'price' => 10
         ]);
         $product->save();
 
         $product = new App\Product([
             'imagePath' => 'http://static.librebook.me/uploads/pics/01/49/841.jpg',
             'title' => 'Smoke Bellew',
-            'description' => 'super cool',
+            'description' => 'Lorem ipsum dolor sit amet, soleat ridens te vis, blandit maiestatis interpretaris usu ei. Pri expetenda vituperatoribus ad, at probo incorrupte inciderint eos. Rebum dissentiunt nec at, ad pro soluta concludaturque, eu vis noster appareat platonem.',
             'price' => 12
         ]);
         $product->save();
 
         $product = new App\Product([
+            'imagePath' => 'http://read-online.in.ua/inf/book/book453/imgbig453.png',
+            'title' => 'Life is a dream',
+            'description' => 'Lorem ipsum dolor sit amet, soleat ridens te vis, blandit maiestatis interpretaris usu ei. Pri expetenda vituperatoribus ad, at probo incorrupte inciderint eos. Rebum dissentiunt nec at, ad pro soluta concludaturque, eu vis noster appareat platonem.',
+            'price' => 10
+        ]);
+        $product->save();
+        $product = new App\Product([
             'imagePath' => 'http://static.librebook.me/uploads/pics/01/49/841.jpg',
             'title' => 'Smoke Bellew',
-            'description' => 'super cool',
+            'description' => 'Lorem ipsum dolor sit amet, soleat ridens te vis, blandit maiestatis interpretaris usu ei. Pri expetenda vituperatoribus ad, at probo incorrupte inciderint eos. Rebum dissentiunt nec at, ad pro soluta concludaturque, eu vis noster appareat platonem.',
             'price' => 12
         ]);
         $product->save();
 
         $product = new App\Product([
-            'imagePath' => 'http://static.librebook.me/uploads/pics/01/49/841.jpg',
-            'title' => 'Smoke Bellew',
-            'description' => 'super cool',
-            'price' => 12
-        ]);
-        $product->save();
-
-        $product = new App\Product([
-            'imagePath' => 'http://static.librebook.me/uploads/pics/01/49/841.jpg',
-            'title' => 'Smoke Bellew',
-            'description' => 'super cool',
-            'price' => 12
+            'imagePath' => 'http://read-online.in.ua/inf/book/book453/imgbig453.png',
+            'title' => 'Life is a dream',
+            'description' => 'Lorem ipsum dolor sit amet, soleat ridens te vis, blandit maiestatis interpretaris usu ei. Pri expetenda vituperatoribus ad, at probo incorrupte inciderint eos. Rebum dissentiunt nec at, ad pro soluta concludaturque, eu vis noster appareat platonem.',
+            'price' => 10
         ]);
         $product->save();
     }
