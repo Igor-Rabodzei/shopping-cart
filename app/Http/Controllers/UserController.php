23032b36eb02d52bb4e08a0789b93040b1fce019
Igor Rabodzei <igor.rabodzei@gmail.com>
2017-10-20 17:03:31 +0300

    third

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,4 +46,9 @@ class UserController extends Controller
     public function getProfile(){
         return view('user.profile');
     }
+
+    public function getLogout(){
+        Auth::logout();
+        return redirect()->back();
+    }
 }
