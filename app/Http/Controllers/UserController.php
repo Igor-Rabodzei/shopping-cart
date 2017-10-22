255a1411adb00797c0d76983a60fb9cbd8f9b516
Igor Rabodzei <igor.rabodzei@gmail.com>
2017-10-22 23:53:34 +0300

    fifth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,7 +25,7 @@ class UserController extends Controller
 
         $user->save();
 
-        return redirect()->route('product.index');
+        return redirect()->route('user.profile');
     }
 
     public function getSignin(){
