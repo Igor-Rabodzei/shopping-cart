adf72d71f97898a42c3e2b80a89f580073b06f31
Igor Rabodzei <igor.rabodzei@gmail.com>
2017-11-04 18:06:29 +0200

    create order

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Cart;
 use App\Product;
+use App\Order;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Session;
+use Auth;
+use Stripe\Charge;
+use Stripe\Stripe;
 
 class ProductController extends Controller
 {
@@ -46,5 +50,41 @@ class ProductController extends Controller
         $cart = new Cart($oldCart);
         $total = $cart->totalPrice;
         return view('shop.checkout', ['total' => $total]);
+    }
+
+    public function postCheckout(Request $request){
+        if (!Session::has('cart')){
+            return redirect()->route('shop.shoppingCart');
+        }
+        $oldCart = Session::get('cart');
+        $cart = new Cart($oldCart);
+
+        Stripe::setApiKey('sk_test_Q1BvfC8Ly7nyTqYkObGjNMou');
+        try{
+            $charge = Charge::create(array(
+                "amount" => $cart->totalPrice * 100,
+                "currency" => "usd",
+                "source" => $request->input('stripeToken'),
+                "description" => "Test Charge"
+            ));
+//            $customer = Charge::create(array(
+//                'description' => "I want to pay for this",
+//                'source' => 'tok_mastercard'
+//            ));
+
+
+            $order = new Order();
+            $order->cart = serialize($cart);
+            $order->address = $request->input('address');
+            $order->name = $request->input('name');
+            $order->payment_id = $charge->id;
+
+            Auth::user()->orders()->save($order);
+        }catch (\Exception $e){
+            return redirect()->route('checkout')->with('error', $e->getMessage());
+        }
+
+        Session::forget('cart');
+        return redirect()->route('product.index')->with('success', 'Successfully purchased products!');
     }
 }
--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -9,19 +9,20 @@
         <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
             <h1>Checkout</h1>
             <h2>Your Total: ${{$total}}</h2>
+            <div id="charge-error" class="alert alert-danger {{ !Session::has('error') ? 'hidden' : '' }}"> {{Session::get('error')}}</div>
             <form action="{{route('checkout')}}" method="post" id="checkout-form">
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="form-group">
                             <label for="name">Name</label>
-                            <input type="text" id="name" class="form-control" required>
+                            <input type="text" id="name" class="form-control" required name="name">
                         </div>
                     </div>
 
                     <div class="col-xs-12">
                         <div class="form-group">
                             <label for="address">Address</label>
-                            <input type="text" id="address" class="form-control" required>
+                            <input type="text" id="address" class="form-control" required name="address">
                         </div>
                     </div>
 
@@ -69,4 +70,12 @@
             </form>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+    <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+
+    <script type="text/javascript" src="{{ URL::to('src/js/checkout.js')}}"></script>
+    {{--<script type="text/javascript" src="https://js.stripe.com/v2/"></script>--}}
+    {{--<script src="https://js.stripe.com/v3/"></script>--}}
 @endsection
